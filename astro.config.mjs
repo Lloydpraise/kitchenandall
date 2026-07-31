b106754eff148aec694390eb9a5abe9023c564d3
mishell100 <mishellewanjiku1@gmail.com>
2026-07-31 13:11:43 +0300

    netlify ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
- 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   site: 'https://YOUR-DOMAIN-HERE.com',
   integrations: [react()],
   vite: {
